a0aa6a783c53e6ed5e483105c169013568b0d5a0
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-01-29 11:19:38 +0430

    get recipe section added

--- src/components/ChefMain.jsx
+++ src/components/ChefMain.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 export default function ChefMain() {
-
   const [ingredients, setIngredients] = React.useState([]);
 
   function addIngredient(formData) {
-   const ingredient = formData.get('ingredient');
-   setIngredients((ingre)=>[...ingre,ingredient])
+    const ingredient = formData.get("ingredient");
+    setIngredients((ingre) => [...ingre, ingredient]);
   }
   const ingredientsItems = ingredients.map((item) => (
     <li key={item}>{item}</li>
@@ -27,6 +26,10 @@ export default function ChefMain() {
         <h1>Ingredients List:</h1>
         <ul>{ingredientsItems}</ul>
       </div>
+     {ingredients.length >0 ? <section className="get-recipe">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, dolore est. Odio consequatur commodi iusto, temporibus?</p>
+        <button>Get Recipe</button>
+      </section> : null}
     </main>
   );
 }
